2fbb2d3bf458200b1d03caceaf1d5b3cdd4370ef
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-09-20 21:45:16 +0200

    Added program flow to main and methods to ConsoleOperations

--- FrequencyAllocationApp/ConsoleOperations.cs
+++ FrequencyAllocationApp/ConsoleOperations.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FrequencyAllocationLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,46 @@ namespace FrequencyAllocationApp
 {
     public class ConsoleOperations
     {
+        public static void WelcomeUser()
+        {
+            Console.WriteLine("Welcome to the Frequency Allocation Utility");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine();
+        }
+
+        public static void DisplayAppInfo()
+        {
+            Console.WriteLine("This app servers as a utility to allocate frquencies to cell sites:");
+            Console.WriteLine("1) Upload a csv file with Cell ID, Easting and Northing values into the bin directory");
+            Console.WriteLine("Example:");
+            Console.WriteLine("Cell ID, Easting, Northing");
+            Console.WriteLine("A,536660,183800");
+            Console.WriteLine("2) Utility will process data and assign frequencies (110, 111, 112, 113, 114, 115)");
+            Console.WriteLine("3) Cell sites and their allocated frequency will be displayed");
+            Console.WriteLine();
+        }
+
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine($"{message}");
+        }
+
+        public static string GetStringInput()
+        {
+            string fileText = Console.ReadLine();
+
+            return fileText;
+        }
+
+        public static void ShowUserImportedData(List<CellModel> cells)
+        {
+            Console.WriteLine("The following data has been imported:");
+            foreach (CellModel cell in cells)
+            {
+                Console.WriteLine($"Cell {cell.ID}: {cell.Easting},{cell.Northing}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
--- FrequencyAllocationApp/Program.cs
+++ FrequencyAllocationApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FrequencyAllocationLibrary;
+using FrequencyAllocationLibrary.Models;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,35 @@ namespace FrequencyAllocationApp
     {
         static void Main(string[] args)
         {
+            // WelcomeUser
+            ConsoleOperations.WelcomeUser();
+
+            // App Info
+            ConsoleOperations.DisplayAppInfo();
+
+            // DisplayMessage for name of file
+            ConsoleOperations.DisplayMessage("Enter filename to import cell site data: ");
+            // GetStringInput for name of file
+            string fileName = ConsoleOperations.GetStringInput();
+
+            // LoadCsvData
+            List<CellModel> cells = FrequencyAllocationOperations.LoadCsvData(fileName);
+            ConsoleOperations.ShowUserImportedData(cells);
+
+            // CalculateConflictDistance
+            ConsoleOperations.DisplayMessage("Calculating Conflict distances of cells...");
+
+
+            // CreateConflictGraph
+            ConsoleOperations.DisplayMessage("Calculating Conflict graph of cells...");
+
+            // FrequencyAllocation
+            ConsoleOperations.DisplayMessage("Allocating frequencies to cells...");
+
+            // DisplayAllocatedFrequencies
+            ConsoleOperations.DisplayMessage("Frequency Allocation completed.");
+
+            // Process another cell site?
         }
     }
 }
